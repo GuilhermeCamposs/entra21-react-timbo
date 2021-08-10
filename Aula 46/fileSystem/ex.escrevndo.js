aa692647c05ca78a2c7408469cea6d54b014aa9c
GUILHERME AUGUSTO CAMPOS <guilherme.campos1@alunos.sc.senac.br>
2021-08-10 10:54:13 -0300

    oi

---
+++ Aula 46/fileSystem/ex.escrevndo.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+ /* 1. Crie um script que irá salvar as informações de memória a cada 5 segundos em um arquivo chamado log.txt
+
+As informações devem ser salvas no seguinte formato:
+{"total_memory":"xx MB", "free_memory":"xx MB", "usage":"xx %"}
+
+Onde:
+    * total_memory: Quantidade total de memória.
+    * free_memory: Quantidade de memória livre.
+    * usage: Porcentagem de uso da memória.
+*/
